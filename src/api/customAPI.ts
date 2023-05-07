2eed6dee0669dae6e8293b2c40b35e90a9402ce8
1yoouoo <1yoouoo@gmail.com>
2023-05-07 15:51:03 +0900

    feat: setup axios & add instance axios

---
+++ src/api/customAPI.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import axios from 'axios';
+
+const API_BASE_URL = '';
+
+const axiosApi = ({ options }: any) => {
+  const instance = axios.create({
+    baseURL: API_BASE_URL,
+    ...options,
+  });
+
+  instance.interceptors.response.use(
+    (response) => {
+      console.log('interceptor > response', response);
+      return response;
+    },
+    (error) => {
+      console.log('interceptor > error', error);
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.request.use(
+    (request) => {
+      console.log('interceptor > request', request);
+      return request;
+    },
+    (error) => {
+      console.log('interceptor > error', error);
+      return Promise.reject(error);
+    }
+  );
+
+  instance.defaults.timeout = 2000; // 2초
+  return instance;
+};
+
+export const defaultInstance = axiosApi(API_BASE_URL);
