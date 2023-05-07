57f30b6fc57eeb8da95d0e56f0821b28b32eb9e1
1yoouoo <1yoouoo@gmail.com>
2023-05-07 15:30:10 +0900

    feat: create svg & styled

--- src/assets/svg/GoBackSvg.tsx
+++ src/assets/svg/GoBackSvg.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import SvgIcon from './SvgIcon';
+import { useRouter } from 'next/router';
+import { SvgIcon } from './SvgIcon';
 
 const GoBackSvg = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
+
   return (
-    <SvgIcon xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'>
+    <SvgIcon viewBox='0 0 320 512' onClick={handleClick}>
       <path d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z' />
     </SvgIcon>
   );
--- src/assets/svg/SvgIcon.tsx
+++ src/assets/svg/SvgIcon.tsx
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 
-const SvgIcon = styled.svg`
+export const SvgIcon = styled.svg`
+  display: flex;
+  justify-content: center;
+  margin: auto;
   width: 24px;
   height: 24px;
   cursor: pointer;
 `;
 
-export default SvgIcon;
+export const SvgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  a {
+    width: 24px;
+    svg {
+      &:hover {
+        fill: #4daa7f;
+      }
+    }
+  }
+`;
