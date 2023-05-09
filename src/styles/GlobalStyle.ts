829e5aa93fde800191ed961f472d3eef67221294
1yoouoo <1yoouoo@gmail.com>
2023-05-09 22:43:36 +0900

    feat: setup variables for global style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,7 +1,53 @@
+// src/styles/GlobalStyle.ts
 import { createGlobalStyle } from 'styled-components';
+import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
-  
+  // variables
+  :root {
+    --font-color-primary: ${theme.fontColor.primary};
+    --font-color-base: ${theme.fontColor.base};
+    --font-color-light: ${theme.fontColor.light};
+    --font-color-lighter: ${theme.fontColor.lighter};
+
+    --font-size-xxxl: ${theme.fontSize.xxxl};
+    --font-size-xxl: ${theme.fontSize.xxl};
+    --font-size-xl: ${theme.fontSize.xl};
+    --font-size-lg: ${theme.fontSize.lg};
+    --font-size-md: ${theme.fontSize.md};
+    --font-size-sm: ${theme.fontSize.sm};
+    --font-size-xs: ${theme.fontSize.xs};
+
+    --button-color-primary: ${theme.buttonColor.primary};
+    --button-color-default: ${theme.buttonColor.default};
+    --button-color-hover: ${theme.buttonColor.hover};
+    --button-color-click: ${theme.buttonColor.click};
+
+    --background-color-default: ${theme.backgroundColor.default};
+    --background-color-popup: ${theme.backgroundColor.popup};
+
+    --border-color-black: ${theme.borderColor.black};
+    --border-color-darkGray: ${theme.borderColor.darkGray};
+    --border-color-gray: ${theme.borderColor.gray};
+    --border-color-lightGray: ${theme.borderColor.lightGray};
+    --border-color-lighterGray: ${theme.borderColor.lighterGray};
+
+    --default-width: ${theme.default.width};
+
+    --svg-width-xxl: ${theme.SvgIconSize.xxl};
+    --svg-height-xxl: ${theme.SvgIconSize.xxl};
+    --svg-width-md: ${theme.SvgIconSize.md};
+    --svg-height-md: ${theme.SvgIconSize.md};
+    --svg-color-default: ${theme.SvgIconColor.default};
+    --svg-color-hover: ${theme.SvgIconColor.hover};
+    --svg-color-click: ${theme.SvgIconColor.click};
+
+    --hashtag-color-text: ${theme.hashTagColor.textColor};
+    --hashtag-color-border: ${theme.hashTagColor.borderColor};
+    --hashtag-color-background: ${theme.hashTagColor.backgroundColor};
+    --hashtag-color-hover: ${theme.hashTagColor.borderHoverColor};
+
+  }
 `;
 
 export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,59 @@
+const theme = {
+  default: {
+    width: '375px',
+  },
+  fontColor: {
+    primary: '#4DAA7F',
+    base: '#666',
+    light: '#888',
+    lighter: '#aaa',
+  },
+  fontSize: {
+    xxxl: '28px',
+    xxl: '24px',
+    xl: '20px',
+    lg: '16px',
+    md: '12px',
+    sm: '10px',
+    xs: '8px',
+  },
+  buttonColor: {
+    primary: '#4DAA7F',
+    default: '#C8C8C8',
+    hover: '#4DAA7F',
+    click: '#9A9A9A',
+  },
+  backgroundColor: {
+    default: '#FFFFFF',
+    popup: '#F0F0F0',
+  },
+  borderColor: {
+    black: '#000000',
+    darkGray: '#333333',
+    gray: '#666666',
+    lightGray: '#999999',
+    lighterGray: '#CCCCCC',
+  },
+  SvgIconSize: {
+    xxxl: '28px',
+    xxl: '24px',
+    xl: '20px',
+    lg: '16px',
+    md: '14px',
+    sm: '10px',
+    xs: '8px',
+  },
+  SvgIconColor: {
+    default: '#858585',
+    hover: '#4DAA7F',
+    click: '#4DAA7F',
+  },
+  hashTagColor: {
+    backgroundColor: '#fff',
+    textColor: '#888',
+    borderColor: '#888',
+    borderHoverColor: '#4DAA7F',
+  },
+};
+
+export default theme;
