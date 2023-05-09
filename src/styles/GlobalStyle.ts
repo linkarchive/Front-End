62ea365bec8a1e523b776aa2c126a1e7914fb48d
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-09 18:41:17 +0900

    refactor: move to global.css

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,27 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    width: 375px;
-  }
-
-  html, body {
-    font-family: "Pretendard";
-    font-weight: 400;
-    height: 100%;
-    margin: auto;
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard.woff2') format('woff2'),
-         url('/fonts/Pretendard.woff') format('woff');
-    font-weight: 100 900;
-    font-style: normal;
-    font-display: block;
-  }
+  
 `;
 
 export default GlobalStyle;
