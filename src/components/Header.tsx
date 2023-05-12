7ef98e3366f99ce080cac601e391b51db9675f88
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-12 15:29:22 +0900

    design: update header design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,43 @@
 import GoBackBtn from '@/components/GoBackBtn';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <header>
+    <Wrapper>
       <nav>
+        {/* FIXME 뒤로가기 버튼 크기 조정 */}
         <GoBackBtn />
-        Header
+        <span className='title'>Header</span>
       </nav>
-    </header>
+    </Wrapper>
   );
 };
 
 export default Header;
+
+const Wrapper = styled.header`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  height: 48px;
+  padding: 0 26px;
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .title {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 0);
+
+    color: --var(--font-color-darkgray);
+    text-align: center;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 21px;
+  }
+`;
