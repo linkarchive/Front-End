481d5d83c46feba24eff9bace52899f32fcc0ee2
1yoouoo <1yoouoo@gmail.com>
2023-05-13 01:45:00 +0900

    chore: test VERCEL_URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-const domain = process.env.NEXT_PUBLIC_DOMAIN || process.env.NEXT_PUBLIC_VERCEL_URL;
+const domain = process.env.NEXT_PUBLIC_VERCEL_URL;
 const RedirectUri = `${domain}/auth/kakao`;
 const ClientId = `${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}`;
 const KakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${ClientId}&redirect_uri=${RedirectUri}&response_type=code`;
