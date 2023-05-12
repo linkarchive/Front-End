6920133f7a797cfb5cdd7b167cd66e9d84079eb4
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-12 15:51:23 +0900

    feat: create link create page

--- src/components/Home/CreateBtn.tsx
+++ src/components/Home/CreateBtn.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const CreateBtn = () => {
   return (
     <Wrapper>
-      <Link href='/'>
+      <Link href='/create'>
         <div className='button'>
           <span>
             <Image src='/assets/svg/add.svg' alt='' width={20} height={20} />
