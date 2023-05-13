f32b25fdaafbd0e94cb37e580aa24e51e5f1891c
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-13 10:59:05 +0900

    feat: set react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MainLayout from '@/layouts/MainLayout';
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
@@ -5,12 +6,16 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from '@/styles';
 import wrapper from '@/store';
 
+const queryClient = new QueryClient();
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <MainLayout>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </MainLayout>
     </ThemeProvider>
   );
