015b5374d816a6eaefa66a6de93f9faa44ea40e3
1yoouoo <1yoouoo@gmail.com>
2023-05-17 21:25:51 +0900

    tmp

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -2,7 +2,6 @@ import { useAppDispatch } from '@/store';
 import { routerSlice } from '@/store/slices/routerSlice';
 import Link from 'next/link';
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
 
 const userId = 1;
 
