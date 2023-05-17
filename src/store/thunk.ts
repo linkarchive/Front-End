0d620d0478ac7646f696378c2031970bbd48f299
1yoouoo <1yoouoo@gmail.com>
2023-05-17 20:21:01 +0900

    feat: Implement redux-thunk

---
+++ src/store/thunk.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+export const thunk = (store) => (next) => (action) => {
+  if (typeof action === 'function') {
+    console.group(`Dispatching a function action: ${action.name || action.toString()}`);
+    console.log('Action:', action);
+    console.log('State before:', store.getState());
+    action(store.dispatch, store.getState);
+    console.log('State after:', store.getState());
+    console.groupEnd();
+  } else {
+    console.group(`Dispatching a regular action: ${action.type}`);
+    console.log('Action:', action);
+    console.log('State before:', store.getState());
+    next(action);
+    console.log('State after:', store.getState());
+    console.groupEnd();
+  }
+};
