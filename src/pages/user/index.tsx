fd7c4a261770406449917d75d0254bbcb51d520d
1yoouoo <1yoouoo@gmail.com>
2023-05-17 23:58:42 +0900

    tmp

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch } from '@/store';
 import { routerSlice } from '@/store/slices/routerSlice';
 import Link from 'next/link';
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
 const userId = 1;
 
@@ -14,7 +15,7 @@ const User = () => {
 
   return (
     <>
-      <Link href={`user/${userId}/profile`}>
+      <Link href='user/profile'>
         <h3>프로필</h3>
       </Link>
       <Link href=''>
@@ -23,8 +24,14 @@ const User = () => {
       <Link href=''>
         <h3>계정</h3>
       </Link>
+      <LogoutHeading onClick={() => console.log('logout!')}>로그아웃</LogoutHeading>
     </>
   );
 };
+
+const LogoutHeading = styled.h3`
+  color: #f45050;
+  cursor: pointer;
+`;
 
 export default User;
