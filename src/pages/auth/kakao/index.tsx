703273e7b5f0348b89fc52a3fc4ee4356f19671e
1yoouoo <1yoouoo@gmail.com>
2023-05-17 16:17:01 +0900

    feat: add promise logic for async

--- src/pages/auth/kakao/index.tsx
+++ src/pages/auth/kakao/index.tsx
@@ -11,8 +11,25 @@ const KakaoAuth = () => {
         const code = router.query.code as string;
         const response = await API.kakaoLogin({ code });
         const token = response.data.accessToken;
-        localStorage.setItem('accessToken', token);
-        router.back();
+
+        // localStorage에 토큰을 저장하는 Promise 생성
+        const storagePromise = new Promise((resolve, reject) => {
+          localStorage.setItem('accessToken', token);
+          if (localStorage.getItem('accessToken')) {
+            resolve(true);
+          } else {
+            reject(new Error('토큰 저장 실패'));
+          }
+        });
+
+        // 토큰 저장이 완료되면 이전 페이지로 이동
+        storagePromise
+          .then(() => {
+            router.back();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       }
     };
 
