be5f506cf20330d048c90a02690ccd5b15df11a8
1yoouoo <1yoouoo@gmail.com>
2023-05-17 21:26:38 +0900

    test: update kakao auth

--- src/pages/auth/kakao/index.tsx
+++ src/pages/auth/kakao/index.tsx
@@ -9,32 +9,12 @@ const KakaoAuth = () => {
     const handleKakaoLogin = async () => {
       if (router.query.code) {
         const code = router.query.code as string;
-        const response = await API.kakaoLogin({ code });
-        const token = response.data.accessToken;
-
-        // localStorage에 토큰을 저장하는 Promise 생성
-        const storagePromise = new Promise((resolve, reject) => {
-          localStorage.setItem('accessToken', token);
-          if (localStorage.getItem('accessToken')) {
-            resolve(true);
-          } else {
-            reject(new Error('토큰 저장 실패'));
-          }
-        });
-
-        // 토큰 저장이 완료되면 이전 페이지로 이동
-        storagePromise
-          .then(() => {
-            router.back();
-          })
-          .catch((error) => {
-            console.error(error);
-          });
+        await API.kakaoLogin({ code });
       }
     };
 
     handleKakaoLogin();
-  }, [router]);
+  }, [router.query]);
 
   return (
     <>
