a02928dd96c30935e1d4edb05b5f4db0e00ecb0c
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-22 05:09:49 +0900

    feat: explore infinity scroll

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,7 +1,11 @@
-import { LinkItemWithProfile } from '@/components/LinkItem';
+import API from '@/api/API';
+import LinkItemList from '@/components/LinkItem/LinkItemLits';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import { useAppDispatch } from '@/store';
 import { routerSlice } from '@/store/slices/routerSlice';
-import React, { useEffect } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React, { useCallback, useEffect, useRef } from 'react';
+import styled from 'styled-components';
 
 const Explore = () => {
   const dispatch = useAppDispatch();
@@ -10,15 +14,50 @@ const Explore = () => {
     dispatch(routerSlice.actions.loadExplorePage());
   }, [dispatch]);
 
+  const target = useRef(null);
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ['archive'],
+    queryFn: ({ pageParam = 0 }) => {
+      return API.getLinksArchive(pageParam);
+    },
+    getNextPageParam: (lastPage_) => {
+      const lastPage = lastPage_.data; // lastPage_.data.result.linkArchive; /** 실제 리소스 */
+      const lastItem = lastPage[lastPage.length - 1].urlId; // lastPage[lastPage.length - 1].urlId; /** 실제 리소스 */
+      const isNextPage = true; // TODO 불러올 데이터 유무 추가
+
+      return isNextPage ? lastItem : undefined;
+    },
+    retry: 1,
+  });
+  const linkList = data?.pages || []; // data?.pages[0].data.result?.linkArchive || [];  /** 실제 리소스 */
+
+  const handleIntersection = useCallback(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !isFetchingNextPage) {
+          hasNextPage && fetchNextPage();
+        }
+      });
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage]
+  );
+  useIntersectionObserver({ target, onIntersection: handleIntersection });
+
   return (
-    <>
-      <LinkItemWithProfile />
-      <LinkItemWithProfile />
-      <LinkItemWithProfile />
-      <LinkItemWithProfile />
-      <LinkItemWithProfile />
-    </>
+    <Wrapper>
+      {linkList.map(({ data: items }) => (
+        <LinkItemList key={items[0].urlId} linkList={items} />
+      ))}
+      {isFetchingNextPage && <div>로딩중...</div>}
+      <div ref={target}>마지막</div>
+    </Wrapper>
   );
 };
 
 export default Explore;
+
+const Wrapper = styled.div`
+  overflow: scroll;
+  height: 100%;
+`;
