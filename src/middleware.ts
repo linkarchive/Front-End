e387f0d4a8611a99a67459679e3e9326e5e27608
1yoouoo <1yoouoo@gmail.com>
2023-05-23 17:48:27 +0900

    feat: separate home by middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,21 @@ import { ACCESS_TOKEN } from './constants';
 export function middleware(request: NextRequest) {
   console.log('Middleware is running');
   const accessToken = request.cookies.get(ACCESS_TOKEN);
+  const nickname = '1yoouoo';
+  // 비로그인용 둘러보기 페이지 접근가능
+  if (request.nextUrl.pathname === '/archive') {
+    return NextResponse.next();
+  }
+
+  // 홈으로 이동 시
+  if (request.nextUrl.pathname === '/') {
+    // 토큰이 있다면 본인 계정의 홈으로 이동
+    if (accessToken) {
+      return NextResponse.redirect(`${request.nextUrl.origin}/${nickname}`);
+    }
+    // 토큰이 없다면 비로그인 홈으로 이동
+    return NextResponse.next();
+  }
 
   // 토큰이 없는 경우 login 페이지로 리다이렉트
   if (!accessToken) {
@@ -16,5 +31,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/create/:path*', '/explore/:path*', '/user/:path*'],
+  matcher: ['/', '/create/:path*', '/archive/:path*', '/settings/:path*'],
 };
