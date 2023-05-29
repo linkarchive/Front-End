3229e6455c7307591cd645b36ed753197c297b36
1yoouoo <1yoouoo@gmail.com>
2023-05-30 00:35:32 +0900

    feat: update instance

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,17 +1,17 @@
 import { AxiosResponse } from 'axios';
-import { authInstance, defaultInstance } from './customAPI';
+import { instance } from './customAPI';
 import { KakaoType } from './types';
 
 const API = {
   sample: async (params: string): Promise<AxiosResponse> => {
-    const response = await defaultInstance.get(`sample`, {
+    const response = await instance.get(`sample`, {
       params,
     });
     return response;
   },
 
   kakaoLogin: async ({ code }: KakaoType): Promise<AxiosResponse> => {
-    const response = await defaultInstance.post(`auth/kakao`, null, {
+    const response = await instance.post(`auth/kakao`, null, {
       params: {
         code,
       },
@@ -20,7 +20,7 @@ const API = {
   },
 
   getLinkMetadata: async (link: string) => {
-    const response = await authInstance.get(`link/metadata`, {
+    const response = await instance.get(`link/metadata`, {
       params: {
         link,
       },
@@ -36,7 +36,7 @@ const API = {
     thumbnail: string;
     tag: string[];
   }) => {
-    const response = await authInstance.post(`link`, {
+    const response = await instance.post(`link`, {
       ...data,
     });
     return response;
@@ -44,48 +44,51 @@ const API = {
 
   /** 링크 둘러보기 */
   getLinksArchive: async (linkId?: string) => {
-    const response = await defaultInstance.get(`links/archive/public?linkId=${linkId}`);
+    const response = await instance.get(`links/archive/public`, {
+      params: {
+        linkId,
+      },
+    });
     return response;
   },
-
   getAuthLinksArchive: async (linkId?: string) => {
-    const response = await authInstance.get(`links/authentication?linkId=${linkId}`);
+    const response = await instance.get(`links/authentication?linkId=${linkId}`);
     return response;
   },
 
   /** 사용자별 링크 둘러보기 */
   getLinksArchiveByUserId: async ({ userId, linkId }: { userId: string; linkId?: string }) => {
-    const response = await defaultInstance.get(`links/public/user/${userId}?linkId=${linkId}`);
+    const response = await instance.get(`links/public/user/${userId}?linkId=${linkId}`);
     return response;
   },
 
   getAuthLinksArchiveByUserId: async ({ userId, linkId }: { userId: string; linkId?: string }) => {
-    const response = await authInstance.get(`links/authentication/user/${userId}?linkId=${linkId}`);
+    const response = await instance.get(`links/authentication/user/${userId}?linkId=${linkId}`);
     return response;
   },
 
   /** 사용자별 북마크 둘러보기 */
   getMarksArchiveByUserId: async ({ userId, linkId }: { userId: string; linkId?: string }) => {
-    const response = await defaultInstance.get(`mark/links/public/user/${userId}?linkId=${linkId}`);
+    const response = await instance.get(`mark/links/public/user/${userId}?linkId=${linkId}`);
     return response;
   },
 
   getAuthMarksArchiveByUserId: async ({ userId, linkId }: { userId: string; linkId?: string }) => {
-    const response = await authInstance.get(
+    const response = await instance.get(
       `mark/links/authentication/user/${userId}?linkId=${linkId}`
     );
     return response;
   },
 
   createTag: async (tag: string) => {
-    const response = await defaultInstance.post(`tag`, {
+    const response = await instance.post(`tag`, {
       tag,
     });
     return response;
   },
 
   getTagsByUserId: async (userId: string) => {
-    const response = await defaultInstance.get(`tags/userId${userId}`);
+    const response = await instance.get(`tags/userId${userId}`);
     return response;
   },
 
@@ -93,37 +96,37 @@ const API = {
     const formData = new FormData();
     formData.append('image', file);
 
-    const response = await authInstance.patch(`profile-image`, formData);
+    const response = await instance.patch(`profile-image`, formData);
     return response;
   },
 
   getMyProfile: async () => {
-    const response = await authInstance.get(`user`);
+    const response = await instance.get(`user`);
     return response;
   },
 
   getUserProfile: async (userId: number) => {
-    const response = await defaultInstance.get(`user/${userId}`);
+    const response = await instance.get(`user/${userId}`);
     return response;
   },
 
   updateUserProfile: async (name: string, introduce: string) => {
-    const response = await authInstance.patch('user', {
+    const response = await instance.patch('user', {
       name,
       introduce,
     });
     return response;
   },
 
-  updateNickname: async (nickname: string, userId: string) => {
-    const response = await authInstance.patch(`/user/${userId}/nickname`, {
+  updateNickname: async ({ nickname, userId }: { nickname: string; userId: string }) => {
+    const response = await instance.patch(`/user/${userId}/nickname`, {
       nickname,
     });
     return response;
   },
 
   validateNickname: async (nickname: string) => {
-    const response = await defaultInstance.post(`/nickname`, {
+    const response = await instance.post(`/nickname`, {
       nickname,
     });
     return response;
