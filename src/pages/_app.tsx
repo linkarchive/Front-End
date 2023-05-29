e17b71fbb674dfaecfd0fcaacd7d7d204b0313c9
1yoouoo <1yoouoo@gmail.com>
2023-05-29 16:20:02 +0900

    feat: implement redux-persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,23 +4,27 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from '@/styles';
-import wrapper from '@/store';
+import wrapper, { persistor } from '@/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const queryClient = new QueryClient();
 
 const App = ({ Component, ...rest }: AppProps) => {
   const { store, props } = wrapper.useWrappedStore(rest);
+
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <MainLayout>
-          <QueryClientProvider client={queryClient}>
-            <Component {...props.pageProps} />
-          </QueryClientProvider>
-        </MainLayout>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <MainLayout>
+            <QueryClientProvider client={queryClient}>
+              <Component {...props.pageProps} />
+            </QueryClientProvider>
+          </MainLayout>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 };
