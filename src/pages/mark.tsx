8f6dba8a301672c055376d7d2ff7fe864c6596dc
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-31 14:25:52 +0900

    feat: create home, mark page

--- src/pages/mark.tsx
+++ src/pages/mark.tsx
@@ -1,12 +1,10 @@
 import API from '@/api/API';
-import CreateBtn from '@/components/Home/CreateBtn';
 import { LinkItemList } from '@/components/LinkItem';
 import useInfinityScroll from '@/hooks/useInfinityScroll';
 import { useAppDispatch } from '@/store';
 import { routerSlice } from '@/store/slices/routerSlice';
 import { useEffect } from 'react';
 
-// 비로그인 사용자 홈페이지
 const Home = () => {
   const dispatch = useAppDispatch();
 
@@ -15,8 +13,8 @@ const Home = () => {
   }, [dispatch]);
 
   const { pages, target, isFetchingNextPage } = useInfinityScroll({
-    fetchFn: (linkId: string) => API.getUserLinksArchive(linkId),
-    queryKey: ['linkList', 'user', 'link'],
+    fetchFn: (linkId: string) => API.getUserMarksArchive(linkId),
+    queryKey: ['linkList', 'user', 'mark'],
     getNextPageParam: (lastPage_) => {
       if (!lastPage_?.data?.hasNext) return undefined;
       const lastPage = lastPage_.data?.linkList;
@@ -27,7 +25,6 @@ const Home = () => {
 
   return (
     <div>
-      <CreateBtn />
       <LinkItemList data={pages} />
       {isFetchingNextPage && <div>로딩중...</div>}
       <div ref={target} />
