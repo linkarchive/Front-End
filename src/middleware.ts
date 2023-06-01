ec36e65dca3d3f020cb370e59a7ef922cccc4712
1yoouoo <1yoouoo@gmail.com>
2023-06-01 20:55:04 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,34 @@ import { NextRequest, NextResponse } from 'next/server';
 import { ACCESS_TOKEN, NICKNAME } from './constants';
 
 export function middleware(request: NextRequest) {
-  const isLoggedIn = request.cookies.get(ACCESS_TOKEN)?.value;
-  const nickname = request.cookies.get(NICKNAME)?.value || '';
+  const allCookies = request.cookies.getAll();
+
+  const isLoggedIn = allCookies.find((cookie) => cookie.name === ACCESS_TOKEN)?.value;
+  const nickname = allCookies.find((cookie) => cookie.name === NICKNAME)?.value || '';
+
+  const response = NextResponse.next();
 
   if (isLoggedIn) {
     // 닉네임 설정 페이지로 이동 허용
     if (!nickname && request.nextUrl.pathname === '/settings/profile/setnickname') {
-      return NextResponse.next();
+      return response;
     }
     // 닉네임 설정 페이지로 이동
     if (!nickname) {
-      return NextResponse.redirect(`${request.nextUrl.origin}/settings/profile/setnickname`);
+      NextResponse.redirect(`${request.nextUrl.origin}/settings/profile/setnickname`);
+      return response;
     }
     // 로그인 페이지 접근 불가
     if (request.nextUrl.pathname === '/login') {
-      return NextResponse.redirect(`${request.nextUrl.origin}/${nickname}`);
+      NextResponse.redirect(`${request.nextUrl.origin}/${nickname}`);
+      return response;
     }
 
-    return NextResponse.next();
+    return response;
   }
-  return NextResponse.redirect(`${request.nextUrl.origin}/login`);
+
+  NextResponse.redirect(`${request.nextUrl.origin}/login`);
+  return response;
 }
 
 export const config = {
