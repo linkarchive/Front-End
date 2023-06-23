9f88cd1fe51436cb586832870a69bd29ecf639cb
1yoouoo <1yoouoo@gmail.com>
2023-06-23 16:48:46 +0900

    fix: Title style

--- src/components/Title.styled.tsx
+++ src/components/Title.styled.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 const Title = styled.h1<{ color?: string }>`
-  width: 100%;
+  margin-right: 10px;
 
   font-weight: 800;
   font-size: 32px;
   color: ${({ color }) => color || '#3a3a3a'};
+  cursor: pointer;
 `;
 
 export default Title;
