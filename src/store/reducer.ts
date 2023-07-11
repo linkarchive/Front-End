001b4092d1d4fd19bfeb70f1e8f5b559a3788745
1yoouoo <1yoouoo@gmail.com>
2023-07-11 17:45:17 +0900

    feat: add user&hashtag slices

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,11 +3,15 @@ import { HYDRATE } from 'next-redux-wrapper';
 import { routerSlice } from './slices/routerSlice';
 import { toastBarSlice } from './slices/toastBarSlice';
 import { homeSlice } from './slices/homeSlice';
+import { HashTagSlice } from './slices/hashTagSlice';
+import { userSlice } from './slices/userSlice';
 
 const combinedReducer = combineReducers({
   router: routerSlice.reducer,
   home: homeSlice.reducer,
   toastBar: toastBarSlice.reducer,
+  hashTag: HashTagSlice.reducer,
+  user: userSlice.reducer,
 });
 
 const rootReducer: typeof combinedReducer = (state, action) => {
