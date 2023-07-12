5923585ffa5e500bac5d725a010fe77e3d8ba4e8
1yoouoo <1yoouoo@gmail.com>
2023-07-12 17:13:04 +0900

    feat: implement filtering hashtag and enhancement code

--- src/pages/[nickname]/index.tsx
+++ src/pages/[nickname]/index.tsx
@@ -1,36 +1,42 @@
 import { ILinksResponse, LinkItemList } from '@/components/LinkItem';
 import Nav from '@/components/Archive/User/Nav';
-import { useAppDispatch } from '@/store';
+import { RootState, useAppDispatch } from '@/store';
 import { routerSlice } from '@/store/slices/routerSlice';
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import API from '@/api/API';
 import { useRouter } from 'next/router';
 import useInfinityScroll from '@/hooks/useInfinityScroll';
 import ProfileLayout from '@/layouts/ProfileLayout';
 import { NextPageWithLayout } from '../_app';
 import LinkItemListLayout from '@/layouts/LinkItemLayout';
+import { useSelector } from 'react-redux';
+import { HashTagSlice } from '@/store/slices/hashTagSlice';
 
 const User: NextPageWithLayout = () => {
   const dispatch = useAppDispatch();
-
+  const { userLink } = useSelector((state: RootState) => state.nav);
+  const { selectedTagName } = useSelector((state: RootState) => state.hashTag);
   const router = useRouter();
+
   const nickname = (router.query.nickname as string) || '';
-  const [item, setItem] = useState<'link' | 'mark'>('link');
 
-  const fetchLinksFn = setFetchLinksFn({ item });
+  const fetchFn = (id: string) => {
+    const tag = selectedTagName === 'All' ? undefined : selectedTagName;
 
-  const queryKey = ['linkList', nickname, item];
+    return userLink
+      ? API.getLinksArchiveByUserId({ nickname, linkId: id, tag })
+      : API.getMarksArchiveByUserId({ nickname, markId: id, tag });
+  };
+
+  const queryKey = ['linkList', nickname, userLink, selectedTagName];
   const { pages, target, isFetchingNextPage } = useInfinityScroll<ILinksResponse>({
-    fetchFn: (id: string) =>
-      item === 'link'
-        ? fetchLinksFn({ nickname, linkId: id })
-        : fetchLinksFn({ nickname, markId: id }),
+    fetchFn,
     queryKey,
     getNextPageParam: (lastPage_) => {
       const hasNext = lastPage_?.hasNext;
       if (!hasNext) return undefined;
 
-      if (item === 'link') {
+      if (userLink) {
         const lastPage = lastPage_?.linkList;
         const lastItem = lastPage[lastPage.length - 1].linkId;
 
@@ -48,9 +54,13 @@ const User: NextPageWithLayout = () => {
     dispatch(routerSlice.actions.loadUserPage());
   }, [dispatch]);
 
+  useEffect(() => {
+    dispatch(HashTagSlice.actions.setInitialState());
+  }, [dispatch, userLink]);
+
   return (
     <>
-      <Nav handleClick={setItem} />
+      <Nav />
       <LinkItemListLayout>
         <LinkItemList data={pages} queryKey={queryKey} />
       </LinkItemListLayout>
@@ -64,13 +74,4 @@ export default User;
 
 User.getLayout = function getLayout(page: ReactElement) {
   return <ProfileLayout>{page}</ProfileLayout>;
-};
-
-const setFetchLinksFn = ({ item }: { item: 'link' | 'mark' }) => {
-  const fetchFunctions = {
-    link: API.getLinksArchiveByUserId,
-    mark: API.getMarksArchiveByUserId,
-  };
-
-  return fetchFunctions[item];
 };
