c5de76b85aa1bf11c2da0fb7cfd45a237a61c789
dev-hamster <hamsterwave0925@gmail.com>
2023-07-14 01:03:53 +0900

    refactor: export validate hash tag func

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -15,6 +15,7 @@ import Spinner from '@/components/Spinner';
 import { createToastBar } from '@/store/slices/toastBarSlice';
 import HashTagList from '@/components/Create/HashTagList';
 import FavoriteTagList from '@/components/Create/FavoriteTagList';
+import { validateHashTag } from '@/utils/validation';
 
 const defaultErrorMessages = {
   url: '',
@@ -24,9 +25,9 @@ const defaultErrorMessages = {
 
 export const getServerSideProps = withAuth();
 
-const Create = ({ userId, accessToken }: { userId: string; accessToken: string }) => {
+const Create = ({ nickname, accessToken }: { nickname: string; accessToken: string }) => {
   setAccessToken(accessToken);
-  const usernickname = userId; // TODO getTagsByNickname 백엔드 작업 후 userId가 아닌 nickname으로 변경
+  const usernickname = nickname; // TODO getTagsByNickname 백엔드 작업 후 userId가 아닌 nickname으로 변경
 
   const dispatch = useAppDispatch();
 
@@ -277,10 +278,6 @@ const ERROR_MESSAGE = {
     INVALID: '제목을 입력해주세요',
   },
   HASHTAG: {
-    TOO_LONG: '최대 8글자 입력해주세요',
-    TOO_SHORT: '최소 2글자 입력해주세요',
-    NO_SPECIAL: '특수기호는 안돼요',
-    NO_SPACE: '공백을 제거해주세요',
     MAXIMUM: '최대 10개까지 등록할 수 있어요',
   },
 };
@@ -316,30 +313,6 @@ const validateHashTagList = (hashtagList: string[]): string => {
     .filter((errMesg) => errMesg !== '');
 
   return errors[0] || '';
-};
-
-const validateHashTag = (text: string): string => {
-  const whiteSpaceRegex = /\s/;
-  const specialSybmolsRegEx = /^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]+$/;
-  const isShort = text.length < 2; // 최소 2글자
-  const isLong = text.length > 8; // 최대 8글자
-  const hasWhitespace = whiteSpaceRegex.test(text); // 공백 불가
-  const hasSpecialSymbols = !specialSybmolsRegEx.test(text); // 특수기호, 이모지 불가
-
-  if (isShort) {
-    return ERROR_MESSAGE.HASHTAG.TOO_SHORT;
-  }
-  if (isLong) {
-    return ERROR_MESSAGE.HASHTAG.TOO_LONG;
-  }
-  if (hasWhitespace) {
-    return ERROR_MESSAGE.HASHTAG.NO_SPACE;
-  }
-  if (hasSpecialSymbols) {
-    return ERROR_MESSAGE.HASHTAG.NO_SPECIAL;
-  }
-
-  return '';
 };
 
 const validateUrl = (url: string): string => {
