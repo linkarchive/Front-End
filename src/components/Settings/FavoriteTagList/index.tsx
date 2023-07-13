be681098fa794e5fdecd5c55b888ea0496db43fc
dev-hamster <hamsterwave0925@gmail.com>
2023-07-14 01:00:51 +0900

    feat: create favorite tag list component

---
+++ src/components/Settings/FavoriteTagList/index.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { FavoriteTag } from './FavoriteTag';
+import { useQuery } from '@tanstack/react-query';
+import API from '@/api/API';
+
+const Title = styled.p`
+  display: block;
+
+  margin-bottom: 8px;
+
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 21px;
+  color: var(--font-color-darkGray);
+`;
+
+const InputBlock = styled.div`
+  width: 327px;
+  padding: 0 5px;
+  margin: 0 auto;
+`;
+
+export const FavoriteTagList = ({ usernickname }: { usernickname: string }) => {
+  const { data: tagListData } = useQuery({
+    queryKey: ['user', 'tagList', 10],
+    queryFn: () => API.getTagsByNickname({ usernickname, size: 10 }),
+    retry: 1,
+  });
+  const tagList = tagListData?.tagList || [];
+
+  return (
+    <InputBlock>
+      <Title>자주 사용하는 태그</Title>
+      <ul>
+        <FavoriteTag tag='qwer' />
+        <FavoriteTag tag='qwer' />
+        <FavoriteTag tag='qwer' />
+        <FavoriteTag tag='qwer' />
+      </ul>
+    </InputBlock>
+  );
+};
