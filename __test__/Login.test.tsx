0a165a529617fa779908259ea767d26e528d0ba3
1yoouoo <1yoouoo@gmail.com>
2023-07-15 21:19:37 +0900

    feat: update Login.test.tsx

--- __test__/Login.test.tsx
+++ __test__/Login.test.tsx
@@ -1,26 +1,28 @@
 import '@testing-library/jest-dom';
-import Login from '../src/pages/login/index';
 import { screen } from '@testing-library/react';
 import { renderWithProviders } from '@/utils/test-utils';
+import Login from '@/pages/login';
 
-test('로그인 페이지에서 "로그인 해주세요."라는 텍스트가 렌더링 되는지', () => {
-  // ARRANGE
-  renderWithProviders(<Login />);
+describe('로그인 페이지에서', () => {
+  test('"로그인 해주세요."라는 텍스트가 렌더링 되는지', () => {
+    // ARRANGE
+    renderWithProviders(<Login />);
 
-  // ACT
-  const heading = screen.getByText(/로그인 해주세요./);
+    // ACT
+    const heading = screen.getByText(/로그인 해주세요./);
 
-  // ASSERT
-  expect(heading).toBeInTheDocument();
-});
+    // ASSERT
+    expect(heading).toBeInTheDocument();
+  });
 
-test('로그인 페이지에서 "kakao_login" alt를 가진 이미지가 렌더링 되는지', () => {
-  // ARRANGE
-  renderWithProviders(<Login />);
+  test('"kakao_login" alt를 가진 이미지가 렌더링 되는지', () => {
+    // ARRANGE
+    renderWithProviders(<Login />);
 
-  // ACT
-  const kakaoLoginImage = screen.getByAltText('kakao_login');
+    // ACT
+    const kakaoLoginImage = screen.getByAltText('kakao_login');
 
-  // ASSERT
-  expect(kakaoLoginImage).toBeInTheDocument();
+    // ASSERT
+    expect(kakaoLoginImage).toBeInTheDocument();
+  });
 });
