0db6da84922056025d31e3dbd1f3d201524d69e5
1yoouoo <1yoouoo@gmail.com>
2023-07-16 22:53:38 +0900

    feat: add Profile.test.tsx

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -3,26 +3,31 @@ import { rest } from 'msw';
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const handlers = [
-  // 샘플 코드
-  rest.get(`${API_BASE_URL}/가로챌 엔드포인트 작성`, (req, res, ctx) => {
-    return (
-      res(ctx.status(200)),
-      ctx.json([
-        {
-          id: 1,
-          title: '여행',
-        },
-        {
-          id: 2,
-          title: '갑시다',
-        },
-      ])
+  /** 프로필 조회 */
+  rest.get(`${API_BASE_URL}/user`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 1,
+        nickname: 'nickname test',
+        introduce: 'introduce test',
+        profileImageFileName: 'test',
+      })
     );
   }),
 
-  // POST /delete-all-cookies 요청에 대한 핸들러 추가
+  /** 이미지 변경 */
+  rest.patch(`${API_BASE_URL}/profile-image`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ profileImageFileName: '/new-image.png' }));
+  }),
+
+  /** 로그아웃 */
   rest.post(`/api/delete-all-cookies`, (req, res, ctx) => {
-    // 요청을 받으면 200 OK 응답을 반환합니다.
     return res(ctx.status(200), ctx.json({ message: 'Success' }));
+  }),
+
+  /** 프로필 업데이트 */
+  rest.patch(`${API_BASE_URL}/user`, (req, res, ctx) => {
+    return res(ctx.json({ nickname: 'newNickname', introduce: 'newIntroduce' }));
   }),
 ];
