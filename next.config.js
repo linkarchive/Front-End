ee6a5fa37cbab3b9e50e704d7d564722f14b08dc
1yoouoo <1yoouoo@gmail.com>
2023-07-26 12:21:39 +0900

    fix: update next.config.js for adding env

--- next.config.js
+++ next.config.js
@@ -12,4 +12,11 @@ module.exports = {
     domains: ['linkarchive-profile.s3.ap-northeast-2.amazonaws.com'],
     unoptimized: true,
   },
+
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+    NEXT_PUBLIC_KAKAO_CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+  },
 };
