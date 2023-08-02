3c577555eae70f4749686096db8c8a98cbb1c76d
dev-hamster <hamsterwave0925@gmail.com>
2023-08-03 01:40:38 +0900

    feat: fetch follwer list api

--- src/pages/[nickname]/follower/index.tsx
+++ src/pages/[nickname]/follower/index.tsx
@@ -1,21 +1,33 @@
-import FollowingList from '@/components/Archive/Follower/FollwingList';
+import API from '@/api/API';
+import FollowerList from '@/components/Archive/Follower/FollowerList';
 import Tab from '@/components/Archive/Follower/Tab';
 import useTabs from '@/hooks/useFollowerTabs';
+import { useQuery } from '@tanstack/react-query';
 
 const Page = () => {
+  const userId = 3; // TODO userId
   const { activeItem } = useTabs();
+  const { data } = useQuery({
+    queryKey: [activeItem, userId],
+    queryFn: () => API.getFollowerList(userId),
+    retry: 0,
+  });
+
+  const followerList = data?.data || [
+    {
+      userId: 1,
+      nickname: 'asdf',
+      introduce:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas necessitatibus nostrum itaque quae eaque. Adipisci unde doloribus fuga possimus dolorem voluptates perferendis id quia magnam ut? Facilis sint facere odio?',
+      profileImageFileName: 'asdf',
+      isfollow: false,
+    },
+  ];
 
   return (
     <>
       <Tab tabs={[{ text: '팔로워' }, { text: '팔로잉' }]} />
-      <FollowingList />
-      {/* 
-    <InfinityScroll
-      renderList={({ pages }) => <FollowingList />}
-      queryKey={['']}
-      fetchFn={}
-    />
-    */}
+      <FollowerList followerList={followerList} />
     </>
   );
 };
