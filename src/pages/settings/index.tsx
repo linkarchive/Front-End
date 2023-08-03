91531eece91e0ef44a4f8c3d0db9260bd842ab7e
1yoouoo <1yoouoo@gmail.com>
2023-08-03 15:54:57 +0900

    feat: Implement settings page

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,11 +1,18 @@
 import API from '@/api/API';
 import { persistor, useAppDispatch } from '@/store';
 import { routerSlice } from '@/store/slices/routerSlice';
-import Link from 'next/link';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import LinkTo from '../../components/Settings/LinkTo';
+import ProfileCard from '@/components/Settings/ProfileCard';
+import { withAuth, withAuthProps } from '@/lib/withAuth';
+import { setAccessToken } from '@/api/customAPI';
+import { useQuery } from '@tanstack/react-query';
 
-const Settings = () => {
+export const getServerSideProps = withAuth();
+
+const Settings = ({ accessToken, nickname }: withAuthProps) => {
+  setAccessToken(accessToken);
   const dispatch = useAppDispatch();
 
   const Logout = async () => {
@@ -19,32 +26,76 @@ const Settings = () => {
     }
   };
 
+  const { data } = useQuery({
+    queryKey: ['user', nickname],
+    queryFn: () => API.getUserProfile(nickname),
+    enabled: !!nickname,
+  });
+
   useEffect(() => {
     dispatch(routerSlice.actions.loadProfilePage());
-  }, [dispatch]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      <Link href='/settings/profile'>
-        <h3>프로필</h3>
-      </Link>
-      <Link href='/settings/hashtag'>
-        <h3>해시태그 관리</h3>
-      </Link>
-      <Link href=''>
-        <h3>계정</h3>
-      </Link>
-      <Link href='/settings/trash'>
-        <h3>임시 삭제 보관함</h3>
-      </Link>
-      <LogoutHeading onClick={Logout}>로그아웃</LogoutHeading>
+      <Container>
+        <ProfileCard {...data} />
+
+        <Wrapper>
+          <Title>관리</Title>
+          <LinkTo href='/settings/hashtag' title='해시태그 관리' border />
+          <LinkTo href='' title='연결된 SNS 계정' content='카카오계정' border />
+          <LinkTo href='/settings/trash' title='삭제 링크 • 마크 목록' />
+        </Wrapper>
+        <Wrapper>
+          <Title>문의</Title>
+          <LinkTo href='' title='링크 • 마크하는 방법' border />
+          <LinkTo href='' title='자주 묻는 질문' />
+        </Wrapper>
+      </Container>
+      <DangerZone>
+        <DangerContent onClick={Logout}>로그아웃</DangerContent>
+        <DangerContent>서비스탈퇴</DangerContent>
+      </DangerZone>
     </>
   );
 };
 
-const LogoutHeading = styled.h3`
-  color: #f45050;
+const Container = styled.main`
+  padding: 0 16px;
+`;
+
+const Wrapper = styled.section`
+  padding: 10px 0;
+`;
+const Title = styled.h2`
+  padding: 10px 0;
+
+  color: ${({ theme }) => theme.gray.lightGray};
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18.2px;
+`;
+
+const DangerZone = styled.div`
+  width: 100%;
+  border-top: 8px solid #f5f5f5;
+  padding: 10px 16px;
+`;
+
+const DangerContent = styled.h3`
+  padding: 10px 0;
+
+  color: ${({ theme }) => theme.gray.lightGray};
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18.2px;
   cursor: pointer;
+
+  :hover {
+    color: ${({ theme }) => theme.primary.main};
+  }
 `;
 
 export default Settings;
