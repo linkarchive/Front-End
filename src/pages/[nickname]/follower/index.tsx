0ec769148250c5ee5c6a308a5a53277eced97631
dev-hamster <hamsterwave0925@gmail.com>
2023-08-07 03:07:52 +0900

    feat: create full page layout to replace main layout

--- src/pages/[nickname]/follower/index.tsx
+++ src/pages/[nickname]/follower/index.tsx
@@ -2,15 +2,18 @@ import API from '@/api/API';
 import { setAccessToken } from '@/api/customAPI';
 import FollowerList from '@/components/Archive/Follower/FollowerList';
 import Tab from '@/components/Archive/Follower/Tab';
+import Header from '@/components/Header/Header';
 import useTabs from '@/hooks/useFollowerTabs';
 import { withAuth, withAuthProps } from '@/lib/withAuth';
 import { useQuery } from '@tanstack/react-query';
+import { ReactElement } from 'react';
 
 export const getServerSideProps = withAuth();
 
 const Page = ({ accessToken, userId: authUserId }: withAuthProps) => {
   setAccessToken(accessToken);
   const userId = 3; // TODO 조회할 유저의 userId 필요
+  const nickname = '푸바오'; // TODO 조회할 유저의 nickname 필요
   const { activeItem } = useTabs();
   const { data } = useQuery({
     queryKey: [activeItem, userId],
@@ -24,10 +27,15 @@ const Page = ({ accessToken, userId: authUserId }: withAuthProps) => {
 
   return (
     <>
+      <Header title={nickname} />
       <Tab tabs={[{ text: '팔로워' }, { text: '팔로잉' }]} />
       <FollowerList authUserId={authUserId} followerList={followerList} />
     </>
   );
+};
+
+Page.getFullLayout = function getFullLayout(page: ReactElement) {
+  return page;
 };
 
 export default Page;
