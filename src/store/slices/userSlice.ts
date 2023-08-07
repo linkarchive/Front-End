b079e2efd14492cebe5a70c4012d3d0f08a6c9e2
1yoouoo <1yoouoo@gmail.com>
2023-08-07 16:48:12 +0900

    fix: update userSlice

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,8 +1,8 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
-  myId: string | null;
-  myNickname: string | null;
+  myId: number | null;
+  myNickname: number | null;
 }
 
 const initialState: UserState = {
@@ -14,10 +14,10 @@ export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUserId: (state, action: PayloadAction<string | null>) => {
+    setUserId: (state, action: PayloadAction<number | null>) => {
       state.myId = action.payload;
     },
-    setNickname: (state, action: PayloadAction<string | null>) => {
+    setNickname: (state, action: PayloadAction<number | null>) => {
       state.myNickname = action.payload;
     },
   },
@@ -27,7 +27,7 @@ export const userState = (state) => state.user;
 export const { setUserId, setNickname } = userSlice.actions;
 
 export const setUser =
-  ({ myId, myNickname }: { myId: string | null; myNickname: string | null }) =>
+  ({ myId, myNickname }: { myId: number | null; myNickname: number | null }) =>
   (dispatch) => {
     dispatch(setUserId(myId));
     dispatch(setNickname(myNickname));
