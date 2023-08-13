d75c9f90b02b9a803513f1a96e954d43ca8bdea1
dev-hamster <hamsterwave0925@gmail.com>
2023-08-14 04:26:17 +0900

    add: add todo comment

--- src/components/Create/LinkInfo.tsx
+++ src/components/Create/LinkInfo.tsx
@@ -1,3 +1,28 @@
-import { LinkInfo } from '@/components/LinkItem';
+import styled from 'styled-components';
+import { Desc } from '@/components/LinkItem/MetaData/MetaData.steyled';
+import { MetaData } from '@/components/LinkItem/LinkItem.type';
+import Thumbnail from '@/components/LinkItem/MetaData/Thumbnail';
+import { InfoWrapper } from '@/components/LinkItem/LinkItem/LinkItem.styled';
+
+const LinkInfoWrapper = styled(InfoWrapper)`
+  width: 100%;
+`;
+
+const LinkInfoMetaWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+// TODO 디자인 수정
+const LinkInfo = ({ metaThumbnail: thumbnail, metaDescription: description }: MetaData) => {
+  return (
+    <LinkInfoWrapper>
+      <LinkInfoMetaWrapper>
+        <Desc>{description}</Desc>
+      </LinkInfoMetaWrapper>
+      <Thumbnail src={thumbnail} alt='' />
+    </LinkInfoWrapper>
+  );
+};
 
 export default LinkInfo;
