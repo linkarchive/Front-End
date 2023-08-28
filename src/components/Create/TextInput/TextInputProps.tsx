ff84d09fa755237459b26e9ccef6cd0a65c5d401
dev-hamster <123740296+dev-hamster@users.noreply.github.com>
2023-08-28 00:56:04 +0000

    remove: delete not used file

--- src/components/Create/TextInput/TextInputProps.tsx
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes, LabelHTMLAttributes } from 'react';
-
-export interface TextInputProps
-  extends InputHTMLAttributes<HTMLInputElement>,
-    LabelHTMLAttributes<HTMLLabelElement> {
-  isRequired?: boolean;
-  message?: string;
-}
